6e816b7f791d5f8e0c7bb6ec3e64f95b775039e5
Michael Sims <michael.h.sims@gmail.com>
2011-04-06 16:30:51 -0500

    Clean imports

--- src/com/example/FpsTestActivity.java
+++ src/com/example/FpsTestActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
